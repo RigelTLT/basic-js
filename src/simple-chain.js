79de5121f14c8c91a3864be339d6d7ee9905f19c
RigelTLT <kozyrin-di@yandex.ru>
2022-04-27 17:11:54 +0400

    feat: add function task chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.chain.push(value);
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if(typeof position !== 'number' || !Number.isInteger(position)){
+      throw new Error("You can't remove incorrect link!");
+    }
+    return this.chain.splice(position, 1);
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.chain = this.chain.reverse();
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let totalChain = this.chain.join(' )~~( ');
+    totalChain = "( "+totalChain+" )";
+    return totalChain;
   }
 };
 
