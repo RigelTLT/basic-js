fff3334a4b8a6d90ff4336847ad533758fa77ec2
RigelTLT <kozyrin-di@yandex.ru>
2022-04-27 15:42:43 +0400

    feat: complete 3 task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+    if(Object.prototype.toString.call(arr) !== '[object Array]'){
+      throw new Error("'arr' parameter must be an instance of the Array!"); 
+    }
+  let totalarr = [];
+  let arr2 = arr;
+  for (let i = 0; i < arr2.length; i++) {
+    if((i===0 && (arr2[i] === "--double-prev" || arr2[i] === "--discard-prev")) || (i===arr2.length-1 && (arr2[i] === "--discard-next" || arr2[i] === "--double-next"))){
+    }
+    else{
+    switch (arr2[i]) {
+      case "--discard-prev":
+        totalarr.splice(i - 1, 1);
+        break;
+      case "--discard-next":
+        arr2.splice(i + 1, 1);
+        i++;
+        break;
+      case "--double-next":
+        totalarr.push(arr2[i + 1]);
+        break;
+      case "--double-prev":
+        totalarr.push(arr2[i - 1]);
+        break;
+      default:
+        totalarr.push(arr2[i]);
+        break;
+    }}
+    
+  }
+  return totalarr;
 }
 
 module.exports = {
-  transform
+  transform,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  console.log(date);
+ if(!date){
+    return 'Unable to determine the time of year!';
+  }
+  else{
+    
+  if(isNaN(Date.parse(date)) || Object.prototype.toString.call(date) !== "[object Date]" || String(date.getMonth) !== 'function getMonth() { [native code] }'){
+    throw new Error('Invalid date!');
+  }
+  else{
+    try{
+    let formatMounth = date.getMonth(); 
+  if(formatMounth<2 || (formatMounth>10 && formatMounth<12)){
+    return 'winter'
+  }
+  if(formatMounth>1 && formatMounth<5){
+    return 'spring'
+  }
+  if(formatMounth>4 && formatMounth<8){
+    return 'summer'
+  }
+  if(formatMounth>7 && formatMounth<11){
+    return 'autumn'
+  }
+  if(formatMounth>12 && formatMounth<0 ){
+    throw new Error('Invalid date!');
+  }
+  }
+  catch{
+     throw new Error('Invalid date!');
+  }
+}
+}
 }
 
 module.exports = {
